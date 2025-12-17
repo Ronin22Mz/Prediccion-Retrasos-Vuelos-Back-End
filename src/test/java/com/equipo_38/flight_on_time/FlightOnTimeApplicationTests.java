1e510891fd0013327d75b42f7a522a8e54f96315
Eduardo Maravilla <eduardomaravillag@gmail.com>
2025-12-17 15:11:04 -0600

    Creacion de arquitectura proyecto inicial

---
+++ src/test/java/com/equipo_38/flight_on_time/FlightOnTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo_38.flight_on_time;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightOnTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
