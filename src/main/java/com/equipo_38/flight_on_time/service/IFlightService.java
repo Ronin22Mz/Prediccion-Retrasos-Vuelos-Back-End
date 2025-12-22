74c958bf21b38760e77c225b359aa93bf832abb5
Diego <spiderdiego21@hotmail.com>
2025-12-22 15:46:15 -0600

    create(service): create FlightService

--- src/main/java/com/equipo_38/flight_on_time/service/IFlightService.java
+++ src/main/java/com/equipo_38/flight_on_time/service/IFlightService.java
@@ -3,7 +3,7 @@ package com.equipo_38.flight_on_time.service;
 import com.equipo_38.flight_on_time.dto.FlightRequestDTO;
 import com.equipo_38.flight_on_time.dto.PredictionResponseDTO;
 
-public interface FlightService {
+public interface IFlightService {
 
     PredictionResponseDTO getPrediction(FlightRequestDTO flightRequestDTO);
 }
