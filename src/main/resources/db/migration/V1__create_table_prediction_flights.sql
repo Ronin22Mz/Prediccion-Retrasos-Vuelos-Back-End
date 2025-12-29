81105b514233d23d30f63ce229eedba8527fb1ad
Luis Calegari <lcalegari97@gmail.com>
2025-12-29 17:06:59 -0300

    feat(resources): add migration/V1 flyway

---
+++ src/main/resources/db/migration/V1__create_table_prediction_flights.sql
@@ -0,0 +1,19 @@
+CREATE TABLE prediction_flights (
+    id BIGSERIAL PRIMARY KEY,
+
+    creation_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    airline VARCHAR(2) NOT NULL,
+
+    origin VARCHAR(3) NOT NULL,
+
+    destination VARCHAR(3) NOT NULL,
+
+    departure_date TIMESTAMP NOT NULL,
+
+    distance_km DOUBLE PRECISION NOT NULL,
+
+    prediction_result VARCHAR(50) NOT NULL,
+
+    probability DOUBLE PRECISION NOT NULL
+);
