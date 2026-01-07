7e2c85634bc9ce03adc729289f372950009f0efa
Eduardo Maravilla <eduardomaravillag@gmail.com>
2026-01-06 19:45:58 -0600

    feat(integration): implement DataScienceGateway and response mappers

--- src/main/java/com/equipo_38/flight_on_time/service/impl/FlightServiceImpl.java
+++ src/main/java/com/equipo_38/flight_on_time/service/impl/FlightServiceImpl.java
@@ -1,15 +1,14 @@
 package com.equipo_38.flight_on_time.service.impl;
 
-import com.equipo_38.flight_on_time.client.DataScienceClient;
+import com.equipo_38.flight_on_time.client.DataScienceGateway;
 import com.equipo_38.flight_on_time.dto.FlightRequestDTO;
-import com.equipo_38.flight_on_time.dto.PredictionDSRequestDTO;
 import com.equipo_38.flight_on_time.dto.PredictionDSResponseDTO;
 import com.equipo_38.flight_on_time.dto.PredictionResponseDTO;
-import com.equipo_38.flight_on_time.model.FlightStatus;
-import com.equipo_38.flight_on_time.model.PredictionFlight;
-import com.equipo_38.flight_on_time.repository.FlightPredictionRepository;
+import com.equipo_38.flight_on_time.mapper.FlightPredictionMapper;
+import com.equipo_38.flight_on_time.repository.IFlightPredictionRepository;
+import com.equipo_38.flight_on_time.service.IAirlineService;
+import com.equipo_38.flight_on_time.service.ICityService;
 import com.equipo_38.flight_on_time.service.IFlightService;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -19,52 +18,24 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class FlightServiceImpl implements IFlightService {
 
-    private final FlightPredictionRepository flightPredictionRepository;
-    private final DataScienceClient dataScienceClient;
+    private final IFlightPredictionRepository flightPredictionRepository;
+    private final DataScienceGateway dataScienceGateway;
+    private final FlightPredictionMapper flightPredictionMapper;
+    private final IAirlineService airlineService;
+    private final ICityService cityService;
 
     @Override
-    @CircuitBreaker(name = "dsClientPrediction", fallbackMethod = "clientPredictionFallback")
     public PredictionResponseDTO getPrediction(FlightRequestDTO flightRequestDTO) {
-        PredictionDSResponseDTO predictionDSResponseDTO = dataScienceClient.getPrediction(mapToDsRequest(flightRequestDTO));
-        PredictionResponseDTO predictionResponseDTO = mapToClientResponse(predictionDSResponseDTO);
-        savePrediction(flightRequestDTO, predictionResponseDTO);
+        validationData(flightRequestDTO);
+        PredictionDSResponseDTO predictionDSResponseDTO = dataScienceGateway.getPrediction(flightRequestDTO);
+        PredictionResponseDTO predictionResponseDTO = flightPredictionMapper.toClientResponse(predictionDSResponseDTO);
+        flightPredictionRepository.save(flightPredictionMapper.toEntity(flightRequestDTO,predictionResponseDTO));
         return predictionResponseDTO;
     }
 
-    public PredictionResponseDTO clientPredictionFallback(FlightRequestDTO flightRequestDTO, Throwable ex) {
-        log.error("DS Client Prediction unavailable", ex);
-        PredictionResponseDTO fallbackResponse = new PredictionResponseDTO(FlightStatus.DELAYED,0.0);
-        savePrediction(flightRequestDTO, fallbackResponse);
-        return fallbackResponse;
-    }
-
-    private PredictionDSRequestDTO mapToDsRequest(FlightRequestDTO flightRequestDTO) {
-        return new PredictionDSRequestDTO(
-                flightRequestDTO.airline(),
-                flightRequestDTO.origin(),
-                flightRequestDTO.destination(),
-                flightRequestDTO.departureDate().getHour(),
-                flightRequestDTO.departureDate().getDayOfWeek().getValue(),
-                flightRequestDTO.distanceKm()
-        );
-    }
-
-    private PredictionResponseDTO mapToClientResponse(PredictionDSResponseDTO predictionDSResponseDTO) {
-        return new PredictionResponseDTO(
-                predictionDSResponseDTO.prediction() == 0 ? FlightStatus.ON_TIME : FlightStatus.DELAYED,
-                predictionDSResponseDTO.probability()
-        );
-    }
-
-    private void savePrediction(FlightRequestDTO flightRequestDTO, PredictionResponseDTO predictionResponseDTO) {
-        PredictionFlight predictionFlight = new PredictionFlight();
-        predictionFlight.setAirline(flightRequestDTO.airline());
-        predictionFlight.setOrigin(flightRequestDTO.origin());
-        predictionFlight.setDestination(flightRequestDTO.destination());
-        predictionFlight.setDepartureDate(flightRequestDTO.departureDate());
-        predictionFlight.setDistanceKm(flightRequestDTO.distanceKm());
-        predictionFlight.setPredictionResult(predictionResponseDTO.forecast());
-        predictionFlight.setProbability(predictionResponseDTO.probability());
-        flightPredictionRepository.save(predictionFlight);
+    private void validationData(FlightRequestDTO flightRequestDTO) {
+      airlineService.validateAirlineCode(flightRequestDTO.airline());
+      cityService.validateCityCode(flightRequestDTO.origin());
+      cityService.validateCityCode(flightRequestDTO.destination());
     }
 }
