41c84fcdc90cbe522d5f7bca43cb23cea6ce0d14
Eduardo Maravilla <eduardomaravillag@gmail.com>
2026-01-21 13:59:14 -0600

    fix(service): implement DTO projections to resolve LazyInitializationException

--- src/main/java/com/equipo_38/flight_on_time/repository/IAirlineDestinationAirportRepository.java
+++ src/main/java/com/equipo_38/flight_on_time/repository/IAirlineDestinationAirportRepository.java
@@ -1,7 +1,10 @@
 package com.equipo_38.flight_on_time.repository;
 
+import com.equipo_38.flight_on_time.dto.AirportResponseDTO;
 import com.equipo_38.flight_on_time.model.AirlineDestinationAirport;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +12,8 @@ import java.util.List;
 @Repository
 public interface IAirlineDestinationAirportRepository extends JpaRepository<AirlineDestinationAirport, Long> {
     List<AirlineDestinationAirport> findAllByAirlineId(Long idAirline);
+
+    @Query("SELECT new com.equipo_38.flight_on_time.dto.AirportResponseDTO(d.id, d.cityCode, d.cityName) " +
+            "FROM AirlineDestinationAirport airda JOIN airda.destination d WHERE airda.airline.id = :idAirline")
+    List<AirportResponseDTO> findDestinationsDtoByAirlineId(@Param("idAirline") Long idAirline);
 }
