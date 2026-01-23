1a25435d32769f17e554eae8e633f03c1e5910da
Diego <spiderdiego21@hotmail.com>
2026-01-23 00:27:24 -0600

    feat(config): add cors policies

---
+++ src/main/java/com/equipo_38/flight_on_time/config/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.equipo_38.flight_on_time.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${url.frontend.dev}")
+    private String urlFrontendDev;
+    @Value("${url.frontend.web}")
+    private String urlFrontendWeb;
+    @Value("${url.backend.dev}")
+    private String urlBackendDev;
+    @Value("${url.backend.server}")
+    private String urlBackendServer;
+
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                WebMvcConfigurer.super.addCorsMappings(registry);
+                registry.addMapping("/**")
+                        .allowedOrigins(urlFrontendDev, urlFrontendWeb, urlBackendDev, urlBackendServer)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+            }
+        };
+    }
+}
